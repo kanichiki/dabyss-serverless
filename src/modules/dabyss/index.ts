03ddde537c0bb36aa852d77ace3895e5af754770
Ayumu-Nono <ayumu.bete@gmail.com>
2020-10-23 20:36:22 +0900

    small settings for debug

--- src/modules/dabyss/index.ts
+++ src/modules/dabyss/index.ts
@@ -1,11 +1,9 @@
-import { Action } from "./classes/Action";
 import { Discussion } from "./classes/Discussion";
 import { Game } from "./classes/Game";
 import { Group } from "./classes/Group";
 import { User } from "./classes/User";
-import { Vote } from "./classes/Vote";
 
-export { Action, Discussion, Game, Group, User, Vote };
+export { Discussion, Game, Group, User};
 
 export * from "./clients/awsClient";
 export * from "./clients/lineClient";
